09cf6667425e5a3a12bf74f464d20b79d9842892
Guilherme <guilhermebarradasdev@gmail.com>
2022-09-29 16:16:30 -0300

    jogo da forca em C

--- jogoForca.c
+++ jogoForca.c
@@ -4,39 +4,45 @@
 int main()
 {
 
-    char palavrasecreta[20];
-    sprintf(palavrasecreta, "MELANCIA");
+	char palavrasecreta[20];
+	sprintf(palavrasecreta, "MELANCIA");
 
-    int acertou = 0;
-    int enforcou = 0;
+	int acertou = 0;
+	int enforcou = 0;
+	char chutes[26];
+	int tentativas = 0;
 
-    char palpites[26];
-    int tentativas = 0;
+	do
+	{
+		char palpite;
 
-    do
-    {
+		for (int i = 0; i < strlen(palavrasecreta); i++)
+		{
+			int achou = 0; 
+			for (int j = 0; j < tentativas; j++)
+			{
+				if (chutes[j] == palavrasecreta[i])
+				{
+					achou = 1;
+					break;
+				}
+			}
+			if (achou)
+			{
+				printf("%c ", palavrasecreta[i]);
+			}
+			else
+			{
+				printf("_ ");
+			}
+		}
+		printf("\n");
+		scanf(" %c", &palpite);
 
-        for (int i = 0; i < strlen(palavrasecreta); i++)
-        {
-            int achou = 0;
+		chutes[tentativas] = palpite;
+		tentativas++;
 
-            for (int j = 0; j < tentativas; j++)
-            {
-                if (palpites[j] == palavrasecreta[i])
-                {
-                    achou = 1;
-                    break;
-                }
-                if (achou)
-                {
-                    printf("%c ", palavrasecreta[i]);
-                }
-                else
-                {
-                    printf("_ ");
-                }
-            }
-            printf("\n");
-        }
-    } while (!acertou && !enforcou);
+	} while (!acertou && !enforcou);
+
+	printf("%s %c", palavrasecreta, palavrasecreta[19]);
 }
